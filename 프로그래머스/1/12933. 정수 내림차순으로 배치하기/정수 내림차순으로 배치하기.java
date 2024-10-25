b8ef0a46204f8f93fb704eab9dc8396e37476646
HEEHYUN0221 <asw03131@naver.com>
2024-10-25 18:57:14 +0900

    Update 정수 내림차순으로 배치하기.java

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,13 +1,25 @@
-import java.util.*;
-
 class Solution {
-  public long solution(long n) {
-        String[] list = String.valueOf(n).split("");
-        Arrays.sort(list);
-
-        StringBuilder sb = new StringBuilder();
-        for (String aList : list) sb.append(aList);
-
-        return Long.parseLong(sb.reverse().toString());
-  }
+    public long solution(long n) {
+        long answer = 0;
+        int length = (int) (Math.log10(n)+1);
+        long []a = new long[length];
+        for(int i=length-1;i>=0;i--){
+            a[i] = (n%10);
+            n/=10;
+            System.out.println(a[i]);
+        }
+        for (int i = 0; i < length; i++) {
+            for(int j = i+1; j < length; j++) {
+                if(a[i]<a[j]){
+                    long temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<length; i++) {
+            answer = answer*10 + a[i];
+        }
+        return answer;
+    }
 }
