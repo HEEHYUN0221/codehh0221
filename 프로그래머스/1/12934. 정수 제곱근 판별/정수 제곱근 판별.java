145d2d750e59a2eb1ccf3bc1775b12b4fd56463d
HEEHYUN0221 <asw03131@naver.com>
2024-10-25 10:03:58 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.13 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        long result = 0;
+        result = (long) Math.sqrt(n);
+        if(Math.pow(result,2)==n) {
+            answer = (long) Math.pow(result+1,2);
+        }
+        else {
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}
