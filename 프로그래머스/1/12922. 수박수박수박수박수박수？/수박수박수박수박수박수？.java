778e038ea566fbf4e38a33f7de4e439ae0b70570
HEEHYUN0221 <asw03131@naver.com>
2024-11-06 10:31:00 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 50.37 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "수";
+        for(int i =0;i<n-1;i++){ 
+             if (answer.charAt(answer.length() - 1) == '수') {
+                answer += "박";
+            } else {
+                answer += "수";
+            }
+        }
+        return answer;
+    }
+}
