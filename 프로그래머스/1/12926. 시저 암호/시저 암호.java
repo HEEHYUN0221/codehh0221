275efae22f35c560cc66fe58896cb3eaeb8c1fa6
HEEHYUN0221 <asw03131@naver.com>
2024-11-20 19:27:20 +0900

    [level 1] Title: 시저 암호, Time: 5.10 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,30 +1,18 @@
 
-import java.util.Arrays;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(String s, int n) {
-        char[] an = s.toCharArray();
-        for(int i=0;i<an.length;i++) {
-            if(an[i]>=65&&an[i]<=90){
-                if((an[i]+n)>90){
-                    
-                    an[i]=(char) ((n+an[i])%90+64);
-                }
-                else{
-                    an[i]=(char) (n+an[i]);
-                }
-                
-                System.out.println(an[i]);
-            }    
-            if(an[i]>=97&&an[i]<=122){
-                if((an[i]+n)>122){
-                    an[i] = (char) ((n+an[i])%122+96);
-                }
-                else{
-                    an[i]=(char) (n+an[i]);
-                }
-            }
-        }
-        return String.valueOf(an);
-    }
+     return s.chars()
+                .mapToObj(c -> {
+                    if(c>='a'&&c<='z'){
+                        return (char)((c+n-'a')%26+'a');
+                    } else if(c>='A'&&c<='Z'){
+                        return (char)((c+n-'A')%26+'A');
+                    } else {
+                        return (char)c;
+                    }
+                }).map(String::valueOf)
+                .collect(Collectors.joining());
+}
 }
 //65A 90Z 97a 122z
