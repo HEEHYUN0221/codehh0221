eb1ad81b68c53f2b9ad5726eee8a2010ff95b8b5
HEEHYUN0221 <asw03131@naver.com>
2024-12-11 12:12:07 +0900

    [level 1] Title: 문자열 나누기, Time: 40.37 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        char x = s.charAt(0);
+        
+       List<String> list = new ArrayList<>();
+        
+        while(true){ 
+            int cnt1=0;//있을때
+            int cnt0=0;//없을때
+            for(int i =0;i<s.length()-1;i++) {
+            if(x==s.charAt(i)){
+                cnt1++;
+            } else {
+                cnt0++;
+            }
+            if(cnt1==cnt0){
+                list.add(s.substring(0,i+1));
+                s=s.substring(i+1);
+                x=s.charAt(0);
+                break;
+            } 
+            }
+            if(s.length()<=2||cnt1!=cnt0){
+                list.add(s);
+                break;
+            }
+        }
+        
+        for(String a : list){
+            System.out.println(a);
+        }
+      
+        return list.size();
+    }
+}
